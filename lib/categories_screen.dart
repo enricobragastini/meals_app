45f794a00abe63141d846abcfbbee1561fdf8ed6
Enrico <enri.braga@gmail.com>
2022-04-01 11:33:38 +0200

    lezione161

---
+++ lib/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'dummy_data.dart';
+import 'category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES.map((catData) {
+          return CategoryItem(title: catData.title, color: catData.color);
+        }).toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200, // max width of tile elements
+            childAspectRatio: 3 / 2, // height:width relation of tiles
+            crossAxisSpacing: 20, // spacing between column
+            mainAxisSpacing: 20), // and rows
+      ),
+    );
+  }
+}
